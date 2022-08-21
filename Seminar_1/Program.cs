43a4d3632d244c066e25842228a8708796dc5ee4
Yuras <monamurs@gmail.com>
2022-08-21 23:26:44 +1000

    Решили 1 задачу

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Вы ввели число {numberA}");
+
+Console.WriteLine("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Вы ввели число {numberB}");
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+if (numberB > numberA)
+{
+    Console.WriteLine($"max = {numberB}");
+}
