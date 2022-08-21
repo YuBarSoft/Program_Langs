254e998648e0a244a6e156a540468f8ae04286fb
Yuras <monamurs@gmail.com>
2022-08-21 23:41:40 +1000

    Решена задача №4

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("Введите число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"Вы ввели число {numberA}");
+﻿/*
+// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"Вы ввели число {numberB}");
 
 if (numberA > numberB)
 {
@@ -14,3 +15,35 @@ if (numberB > numberA)
 {
     Console.WriteLine($"max = {numberB}");
 }
+*/
+
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"max = {max}");
+*/
+
+
+
+
