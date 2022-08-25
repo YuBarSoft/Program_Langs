c90ab61116d1b41913c1dff9a9349d4a472908e1
Yuras <monamurs@gmail.com>
2022-08-25 23:56:30 +1000

    1 task solved

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Урок 2. Массивы и функции в программировании
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindSecondDigit (int number) // void
+{
+    int dec = number % 100 / 10;
+    return dec;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int secondDigit = FindSecondDigit(randNumber);
+
+Console.WriteLine($"Second Digit in {randNumber} is {secondDigit}");
