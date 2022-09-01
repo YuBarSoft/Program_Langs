9c8a1a213472383c6618027cab24f10355c9c21c
Yuras <monamurs@gmail.com>
2022-09-01 22:24:22 +1000

    edit code

--- Seminar_4/Program.cs
+++ Seminar_4/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿//*
 // Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 Console.Write("Введите число A: ");
@@ -6,21 +6,21 @@ int numberA = Convert.ToInt16(Console.ReadLine());
 Console.Write("Введите число B: ");
 int numberB = Convert.ToInt16(Console.ReadLine());
 
+// можно использовать метод Pow()
+int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+Console.WriteLine($"{numberA}, {numberB} -> {result}");
+
+// можно использовать умножение в цикле
 int FindExponent(int numberA, int numberB)
 {
-    // можно использовать метод Pow()
-    // int result = Convert.ToInt32(Math.Pow(numberA, numberB));
-
-    // и можно использовать умножение в цикле
     int result = 1;  // объявили переменную
+
     for(int i=1; i <= numberB; i++) 
         result = result * numberA;
     
     return result;
 }
-
 int exponent = FindExponent(numberA, numberB);
 Console.WriteLine($"{numberA}, {numberB} -> {exponent}");
-*/
 
 
